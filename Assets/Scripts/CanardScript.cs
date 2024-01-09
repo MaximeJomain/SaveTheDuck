65a46383e4f66c8658e16d882b956d045d879ddd
lance110 <lanceboun@gmail.com>
2024-01-09 10:36:42 +0100

    Déplacement du cannard

--- Assets/Scripts/CanardScript.cs
+++ Assets/Scripts/CanardScript.cs
@@ -6,10 +6,16 @@ public class CanardScript : MonoBehaviour
 {
     public float vieCanardVie = 100;
 
-    public int vitesseCanard = 5;
+    public int vitesseCanard = 1;
 
 
-    public Rigidbody RigidBodyCannard;
+    public Rigidbody RigidBodyCanard;
+    public GameObject Canard;
+
+    private void Awake()
+    {
+        Canard = GameObject.Find("Canard");
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +27,32 @@ public class CanardScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        RigidBodyCannard.velocity = new Vector3(vitesseCanard * Input.GetAxis("Horizontal"), RigidBodyCannard.velocity.y, vitesseCanard * Input.GetAxis("Vertical"));
+        //RigidBodyCannard.velocity = new Vector3(vitesseCanard * Input.GetAxis("Horizontal"), RigidBodyCannard.velocity.y, vitesseCanard * Input.GetAxis("Vertical"));
+
+        if (Canard.transform.position.z >= 0.3f && Canard.transform.position.x > (-0.9f))
+        {
+            RigidBodyCanard.velocity = new Vector3(vitesseCanard * (-0.1f), RigidBodyCanard.velocity.y, RigidBodyCanard.velocity.z);
+            Canard.transform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+
+
+        if (Canard.transform.position.x <= (-0.9f) && Canard.transform.position.z > (-0.3f))
+        {
+            RigidBodyCanard.velocity = new Vector3(RigidBodyCanard.velocity.x, RigidBodyCanard.velocity.y, vitesseCanard * (-0.1f));
+            Canard.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+
+        if (Canard.transform.position.z <= (-0.3f) && Canard.transform.position.x < (0.9f))
+        {
+            RigidBodyCanard.velocity = new Vector3(vitesseCanard * 0.1f, RigidBodyCanard.velocity.y, RigidBodyCanard.velocity.z);
+            Canard.transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        if (Canard.transform.position.x >= (0.9f) && Canard.transform.position.z < (0.3f))
+        {
+            RigidBodyCanard.velocity = new Vector3(RigidBodyCanard.velocity.x, RigidBodyCanard.velocity.y, vitesseCanard * (0.1f));
+            Canard.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
